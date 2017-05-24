967af186d67c2ef3d046352c914845795349dd14
Anniekobia <kobiaannunziata94@gmail.com>
2017-05-24 15:45:57 +0300

    Add files via upload

---
+++ maintainance.php
@@ -0,0 +1,50 @@
+<DOCTYPE html>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Website Maintenance page</title>
+		<style type="text/css">
+			body
+			{
+				text-align: center;
+				position: relative;
+			}
+			#content
+			{
+				margin-top: 13%;
+				margin-right: auto;
+				margin-left: auto;
+				height: 230px;
+			  	width: 750px;
+			  	border-radius: 7px;
+			  	border: 1px;
+			  	padding: 10px;
+			  	align-content: center;
+			}
+			#photo
+			{
+
+				float: left;
+			}
+			#writting
+			{
+				height: 300px;
+				width: 450px;
+				float: right;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="content">
+			<div id="photo">
+				<img id="guyphoto" src="/projectone/images/guy.jpg">
+			</div>
+			<article>
+				<div id="writting">
+					<article><h2>Bliss is temporarily unavailable!</h2></article>
+					<p>Our site is down for maintanace.We apologize for any inconvinience caused. For any feedback or updates on the site visit or facebook page.</p>
+				</div>
+			</article> 
+		</div>
+	</body>
+</html>
